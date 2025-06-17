9eab77efd7b793fd05ed62f239c9c8c1a497811f
phrkdll <benjamin.hotop@gmail.com>
2025-06-17 09:58:09 +0200

    fix tests

--- pkg/player/player_test.go
+++ pkg/player/player_test.go
@@ -3,33 +3,43 @@ package player_test
 import (
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/phrkdll/monomatch/pkg/player"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestNew(t *testing.T) {
 	type testCase struct {
-		name   string
-		player string
-		err    error
+		name     string
+		playerId player.PlayerId
+		player   string
+		err      error
 	}
 
 	testCases := []testCase{
 		{
-			name:   "valid",
-			player: "vlad",
-			err:    nil,
+			name:     "valid",
+			playerId: player.PlayerId{Inner: uuid.NewString()},
+			player:   "vlad",
+			err:      nil,
 		},
 		{
-			name:   "invalid",
-			player: "",
-			err:    player.ErrPlayerNameRequired,
+			name:     "no name",
+			playerId: player.PlayerId{Inner: uuid.NewString()},
+			player:   "",
+			err:      player.ErrPlayerNameRequired,
+		},
+		{
+			name:     "no id",
+			playerId: player.PlayerId{},
+			player:   "bork",
+			err:      player.ErrPlayerNameRequired,
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			p, err := player.New(tc.player, nil)
+			p, err := player.New(tc.playerId, tc.player, nil)
 			assert.Equal(t, tc.err, err)
 
 			if err == nil {
